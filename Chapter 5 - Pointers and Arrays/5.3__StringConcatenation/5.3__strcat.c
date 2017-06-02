a9c3a8a027c72b2bcc0fbee3d9814d799edc1f2d
dev-0509 <devsaini.id@gmail.com>
2017-06-03 00:15:56 +0530

    String Concatenation using Pointers

---
+++ Chapter 5 - Pointers and Arrays/5.3__StringConcatenation/5.3__strcat.c
@@ -0,0 +1,76 @@
+#include "stdio.h"
+#include "string.h"
+#include "Global.h"
+
+void displayResult(int status, char * string) {
+
+	if( status )
+
+		printf("%s", string);
+
+	else
+
+		printf("Concatenation Not Possible!");
+
+}
+
+int checkLengthOfConcat_string(char * source_string, char * concat_string) {
+
+	int source_string_length;
+	int concat_string_length;
+
+	source_string_length = strlen( source_string );
+	concat_string_length = strlen( concat_string );
+
+	if( MAXLENGTH - source_string_length > concat_string_length )
+
+		return CONCAT_POSSIBLE;
+
+	else
+
+		return CONCAT_NOT_POSSIBLE;
+
+}
+
+int str_cat(char * source_string, char * concat_string) {
+
+	int status;
+
+	status = checkLengthOfConcat_string( source_string , concat_string );
+
+	if( status ) {
+
+		source_string += strlen( source_string );
+
+		while( ( *source_string++ = *concat_string++ ) != END_OF_STRING ) ;
+
+		return SUCCESS;
+
+	} else
+
+		return CONCAT_NOT_POSSIBLE;
+
+}
+
+void fetchStringsFromCmd(char const * argv[], char * source_string, char * concat_string) {
+
+	strcpy( source_string , argv[ 1 ] );
+
+	strcpy( concat_string , argv[ 2 ] );
+
+}
+
+int main(int argc, char const * argv[]) {
+
+	char source_string[ MAXLENGTH ], concat_string[ MAXLENGTH ];
+	int status;
+
+	fetchStringsFromCmd( argv , source_string , concat_string );
+
+	status = str_cat( source_string , concat_string );
+
+	displayResult( status , source_string );
+
+	return 0;
+
+}
