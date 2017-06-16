6fb556d38b9b0b579a90aa17225b9e3da9eee60b
dev-0509 <devsaini.id@gmail.com>
2017-06-16 21:11:39 +0530

    5.Improvised Code to removed Illegal Memory Access

--- Chapter 5 - Pointers and Arrays/5.7__ImprovisedReadLines/Global.h
+++ Chapter 5 - Pointers and Arrays/5.7__ImprovisedReadLines/Global.h
@@ -3,7 +3,7 @@
 #define MAXLINES	100
 #define MAXLENGTH	50
 #define OVERFLOW	-1
-#define EOI			'%'
+#define EOI		'%'
 #define NEWLINE		'\n'
 
 #define NEW_CHARACTER_EXISTS(character) (character=getchar())!=EOI&&(character!=NEWLINE)
