e931889fe6b95758c04599e4acf5bfef0f0b04ec
dev-0509 <devsaini.id@gmail.com>
2017-06-26 23:57:11 +0530

    Sorting - Lexicographical | Reversed | Numeric ( Case Insensitive )

---
+++ Chapter 5 - Pointers and Arrays/5.15__CaseInsensitiveSorting/sorting/Makefile.mk
@@ -0,0 +1,8 @@
+sort: 
+
+
+main.o: main.c
+	gcc -c main.c
+
+utility.o: utility.c
+	gcc -c utility.c
---
+++ Chapter 5 - Pointers and Arrays/5.15__CaseInsensitiveSorting/utility.c
@@ -0,0 +1,41 @@
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+#include "header_files/readlines.h"
+#include "header_files/sorthandler.h"
+
+void openFile(FILE ** fptr) {
+
+	char filename[] = "input_file.txt";
+
+	*fptr = fopen( filename , READ );
+
+}
+
+void fetchMemoryForStorage(char ** string_ptr, int length) {
+
+	*string_ptr = ( char * )malloc( length );
+
+}
+
+int checkOverflow(int no_of_lines) {
+
+	if( no_of_lines >= MAXLINES )
+
+		return OVERFLOW;
+
+	return STORAGE_AVAILABLE;
+
+}
+
+void checkMemoryStatus(int status) {
+
+	if( status != SUCCESS ) {
+
+		printf("\nInput too large");
+
+		exit( 0 );
+
+	}
+
+}
