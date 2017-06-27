32ecf8b8a2a5b36063a921e7372a5d58ee9a205f
dev-0509 <devsaini.id@gmail.com>
2017-06-28 01:48:30 +0530

    Sorting - Lexicographical | Reversed | Numeric | Directory Order

--- Chapter 5 - Pointers and Arrays/5.16__DirectorySort/header_files/sortingmethods.h
+++
@@ -1,9 +0,0 @@
-/* --------------------------- FUNCTION HEADERS ------------------------------ */
-
-void convertComparatorStringToLowerCase( char * , char * );
-
-void convertComparableStringToLowerCase( char * , char * );
-
-int compareStrings( char * , char * );
-
-/* ---------------------------------------------------------------------------- */
